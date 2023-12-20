c4b839f2be25cb0dd64a2b814523b0686b54065c
clumsyproninja <128275573+clumsyproninja@users.noreply.github.com>
2023-12-20 03:56:10 -0500

    Fix githubAPI.js

--- js/githubAPI.js
+++ js/githubAPI.js
@@ -1,6 +1,6 @@
 // Variables for ease of access
 const username = 'clumsyproninja'
-const githubAPI = `https://api.github.com/users/${username}/repos?timestamp=${Date.now()}`
+const githubAPI = `https://api.github.com/users/${username}/repos`
 
 // Handle button
 document.getElementById('refresh').addEventListener('click', fetchRepos)
@@ -12,36 +12,40 @@ async function fetchRepos(){
         const response = await fetch(githubAPI)
         const repos = await response.json()
         const reposList = document.getElementById('repos') // Element where repos will display
-
+        
         reposList.innerHTML = '' // Clear existing list
-
+        
         // Loop through each repo and create elements to display them
         repos.forEach(repo => {
             // Variables for ease of access
             const li = document.createElement('li')
             const link = document.createElement('a')
-            const lastUpdate = new Date(repo.updated_at).toLocaleDateString()
+            // const lastUpdate = new Date(repo.updated_at).toLocaleDateString()
             const repoInfo = document.createElement('article')
-            
+
+            // Modify and update the repo timestamps
+            const lastDate = new Date(repo.updated_at)
+            const lastUpdate = lastDate.toLocaleString('en-US', { timeZone: 'UTC'})
+            console.log(lastUpdate)
+            console.log(repo.updated_at)
+
             link.href = repo.html_url // Set the link
             link.target = '_blank' // Open link to new tab
             link.textContent = repo.name // Set the repo name
             repoInfo.appendChild(link)
-
+            
             // Check if repos have descriptions
             if (repo.description){
                 const description = document.createElement('p')
                 // Shorten descriptions if too long
                 const trimmedDesc = repo.description.length > 100 ? repo.description.slice(0,100) + '....' : repo.description
-
+                
                 // Set the content and append
                 description.textContent = trimmedDesc
                 repoInfo.appendChild(description)
             }
-
+            
             // Add repo's last update
-            console.log('repos')
-            console.log('Last update: ', lastUpdate)
             repoInfo.innerHTML += `<span>Last Update: ${lastUpdate}</span>`
             li.appendChild(repoInfo)
             reposList.appendChild(li)
