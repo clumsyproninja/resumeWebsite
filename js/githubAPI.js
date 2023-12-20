8fb8d850a104545409a1405eb9e8f0165d0a851c
clumsyproninja <128275573+clumsyproninja@users.noreply.github.com>
2023-12-19 23:54:46 -0500

    Add comments githubAPI.js

--- js/githubAPI.js
+++ js/githubAPI.js
@@ -1,30 +1,47 @@
+// Variables for ease of access
 const username = 'clumsyproninja'
 const apiUrl = `https://api.github.com/users/${username}/repos`
 
+// Function to fetch and display github repositories
 async function fetchRepos(){
     try{
+        // Fetch data from GitHub API
         const response = await fetch(apiUrl)
         const repos = await response.json()
+        const reposList = document.getElementById('repos') // Element where repos will display
 
-        const reposList = document.getElementById('repos')
-
+        // Loop through each repo and create elements to display them
         repos.forEach(repo => {
+            // Variables for ease of access
             const li = document.createElement('li')
             const link = document.createElement('a')
-            const arrow = document.createTextNode(' → ')
             const lastUpdate = new Date(repo.updated_at).toLocaleDateString()
+            const repoInfo = document.createElement('article')
             
-            link.href = repo.html_url
-            link.target = '_blank'
-            link.textContent = repo.name
+            link.href = repo.html_url // Set the link
+            link.target = '_blank' // Open link to new tab
+            link.textContent = repo.name // Set the repo name
+            repoInfo.appendChild(link)
 
-            li.appendChild(link)
-            li.appendChild(arrow)
-            li.appendChild(document.createTextNode(`Last Update: ${lastUpdate}`))
+            // Check if repos have descriptions
+            if (repo.description){
+                const description = document.createElement('p')
+                // Shorten descriptions if too long
+                const trimmedDesc = repo.description.length > 100 ? repo.description.slice(0,100) + '....' : repo.description
+
+                // Set the content and append
+                description.textContent = trimmedDesc
+                repoInfo.appendChild(description)
+            }
+
+            // Add repo's last update
+            repoInfo.innerHTML += `<span>Last Update: ${lastUpdate}</span>`
+            li.appendChild(repoInfo)
             
             reposList.appendChild(li)
         })
     } catch (error){
+        // Handle errors if fetch fails
         console.error('Error fetching repositories:', error)
     }
 }
